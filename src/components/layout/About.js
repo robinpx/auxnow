5cb9a855b2a70e2fe84af312521851bb84621f4f
robin <robin.rb.luo@gmail.com>
2020-12-22 16:53:11 -0600

    Fix link and size of credits

--- src/components/layout/About.js
+++ src/components/layout/About.js
@@ -9,7 +9,7 @@ class About extends React.Component {
               <p><b>AuxNow</b> is a web app that generates Spotify playlists based off your Liked Songs
               and your chosen genres, mood, and sound textures. </p>
               <p>Made to quickly create background music to aux, share with friends, or for personal use. </p>
-              <p>Brought to you by <a href='https://github.com'>Robin</a> and Spotify API.</p>
+              <p className='credits'>Brought to you by <a href='https://github.com/robinpx'>Robin</a> and Spotify API.</p>
               <div id='close-button' onClick={this.props.close}>⏎</div>
             </div>
           </div>
